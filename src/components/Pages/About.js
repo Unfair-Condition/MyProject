959a3b0a489ecf7d49d1774cece91fd26b8e7c49
Akshat Srivastava <67952921+Unfair-Condition@users.noreply.github.com>
2020-11-02 20:39:41 +0530

    First Commit

---
+++ src/components/Pages/About.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Header from '../Common/Header';
+import image from '../assets/img/about.jpeg';
+import Timeline from '../Common/Timeline';
+import Team from '../Common/Team';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <Header
+                    title="About us"
+                    subtitle="It's really a great story"
+                    showButton={false}
+                    image={image}
+                    
+                    
+                />
+                <Timeline />
+                <Team />
+
+                
+            </div>
+
+
+
+
+
+        )
+    }
+}
+
+
+export default About;
